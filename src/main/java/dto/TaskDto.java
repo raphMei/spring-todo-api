d907df1fd411eebfcdb53a7a749d68f1fe3af4c5
Raphael Meimoun <raphael.meimoun95@gmail.com>
2025-03-24 11:38:13 +0100

    Initialisation du model avec TaskManager et TaskStatus

--- src/main/java/dto/TaskDto.java
+++ src/main/java/dto/TaskDto.java
@@ -1,4 +1,14 @@
 package dto;
 
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class TaskDto {
+    private Long id;
+    private String title;
+    private String description;
 }
--- src/main/java/model/TaskManager.java
+++ src/main/java/model/TaskManager.java
@@ -1,5 +1,44 @@
 package model;
 
+import jakarta.persistence.*;
+import lombok.*;
 
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class TaskManager {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    private TaskStatus status; // en attente, en cours, terminé
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+        updatedAt = LocalDateTime.now();
+        status = TaskStatus.PENDING;
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+
+
 }
